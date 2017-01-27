ba30dfb7f74eede923e18a87615ba6085ff36a9d
Alex Simonides <acsimonides@gmail.com>
2017-01-26 17:21:18 -0800

    Upate to latest version of boilerplate

--- build/dev-server.js
+++ build/dev-server.js
@@ -7,8 +7,8 @@ var webpack = require('webpack');
 var opn = require('opn');
 var proxyMiddleware = require('http-proxy-middleware');
 var webpackConfig = process.env.NODE_ENV === 'testing'
-    ? require('./webpack.prod.conf')
-    : require('./webpack.dev.conf');
+  ? require('./webpack.prod.conf')
+  : require('./webpack.dev.conf');
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port;
@@ -20,29 +20,28 @@ var app = express();
 var compiler = webpack(webpackConfig);
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-        colors: true,
-        chunks: false
-    }
+  publicPath: webpackConfig.output.publicPath,
+  quiet: true
 });
 
-var hotMiddleware = require('webpack-hot-middleware')(compiler);
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+});
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-        hotMiddleware.publish({action: 'reload'});
-        cb()
-    })
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' });
+    cb()
+  })
 });
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
-    var options = proxyTable[context];
-    if (typeof options === 'string') {
-        options = {target: options}
-    }
-    app.use(proxyMiddleware(context, options))
+  var options = proxyTable[context];
+  if (typeof options === 'string') {
+    options = { target: options }
+  }
+  app.use(proxyMiddleware(context, options))
 });
 
 // handle fallback for HTML5 history API
@@ -59,16 +58,20 @@ app.use(hotMiddleware);
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
 app.use(staticPath, express.static('./static'));
 
-module.exports = app.listen(port, function (err) {
-    if (err) {
-        console.log(err);
-        return
-    }
-    var uri = 'http://localhost:' + port;
-    console.log('Listening at ' + uri + '\n');
+var uri = 'http://localhost:' + port;
 
-    // when env is testing, don't need open it
-    if (process.env.NODE_ENV !== 'testing') {
-        opn(uri)
-    }
+devMiddleware.waitUntilValid(function () {
+  console.log('> Listening at ' + uri + '\n')
+});
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  // when env is testing, don't need open it
+  if (process.env.NODE_ENV !== 'testing') {
+    opn(uri)
+  }
 });
