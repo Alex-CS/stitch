660391727693cc40bdf17de41927087cce536692
Alexander Simonides <Alex-CS@users.noreply.github.com>
2022-02-15 13:57:55 -0800

    Add UI toggle between the two modes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,7 @@ export default {
         showSpines: false,
         center: new Point(size / 2, size / 2),
       },
+      mode: 'canvas',
     };
   },
   computed: {
@@ -49,60 +50,96 @@ export default {
 
 <template>
   <div id="app">
-    <svg :width="size" :height="size">
-      <StitchMultiCurve
-        :options="options"
-        :curve-type="curveType"
-        :reps="reps"
-        :radius="radius"
-        :initial="initial"
-        :colors="colors"
-      />
-    </svg>
-    <!--<StitchCanvas :resolution="20" :size="600"/>-->
-    <div class="controls">
-      <!--<div>
-        <label>layerCount</label>
-        <input type="range"
-               v-model="options.layerCount"
-               min="1"
-               max="8">
-        <span>{{ options.layerCount }}</span>
-      </div>
-      <div>
-        <label>layerSepFactor</label>
-        <input type="range"
-               v-model="options.layerSepFactor"
-               min="1"
-               max="8">
-        <span>{{ options.layerSepFactor }}</span>
-      </div>
-      <div>
-        <label>numVertices</label>
-        <input type="range"
-               v-model="options.numVertices"
-               min="3"
-               max="8">
-        <span>{{ options.numVertices }}</span>
-      </div>
-      <div>
-        <label>resolution</label>
-        <input type="range"
-               v-model="options.resolution"
-               min="4"
-               max="64">
-        <span>{{ options.resolution }}</span>
-      </div>
-      <div>
-        <label>rotation</label>
-        <input type="range"
-               v-model="options.rotation"
-               min="0"
-               max="1"
-               :step="1/8">
-        <span>{{ options.rotation * 360 }}</span>
-      </div>-->
+    <div class="ModeToggle">
+      <label>
+        <input
+          v-model="mode"
+          type="radio"
+          name="mode"
+          value="canvas"
+        >
+        Canvas
+      </label>
+      <label>
+        <input
+          v-model="mode"
+          type="radio"
+          name="mode"
+          value="config"
+        >
+        Config
+      </label>
     </div>
+    <StitchCanvas
+      v-if="mode === 'canvas'"
+      :resolution="20"
+      :size="size"
+    />
+    <template v-else>
+      <svg :width="size" :height="size">
+        <StitchMultiCurve
+          :options="options"
+          :curve-type="curveType"
+          :reps="reps"
+          :radius="radius"
+          :initial="initial"
+          :colors="colors"
+        />
+      </svg>
+      <div class="controls">
+        <div>
+          <label>layerCount</label>
+          <input
+            v-model="options.layerCount"
+            type="range"
+            min="1"
+            max="8"
+          >
+          <span>{{ options.layerCount }}</span>
+        </div>
+        <div>
+          <label>layerSepFactor</label>
+          <input
+            v-model="options.layerSepFactor"
+            type="range"
+            min="1"
+            max="8"
+          >
+          <span>{{ options.layerSepFactor }}</span>
+        </div>
+        <div>
+          <label>numVertices</label>
+          <input
+            v-model="options.numVertices"
+            type="range"
+            min="3"
+            max="8"
+          >
+          <span>{{ options.numVertices }}</span>
+        </div>
+        <div>
+          <label>resolution</label>
+          <input
+            v-model="options.resolution"
+            type="range"
+            min="4"
+            max="64"
+          >
+          <span>{{ options.resolution }}</span>
+        </div>
+        <div>
+          <label>rotation</label>
+          <input
+            v-model="options.rotation"
+            type="range"
+            min="0"
+            max="1"
+            :step="1/8"
+          >
+          <span>{{ options.rotation * 360 }}</span>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -113,16 +150,23 @@ body {
   margin: 0;
 }
 #app {
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   padding: 10px;
 }
+
+.ModeToggle {
+  display: flex;
+  gap: 5vw;
+}
+
 svg {
   overflow: auto;
 }
+
 .controls {
   align-self: flex-end;
   display: flex;
