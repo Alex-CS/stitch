ae6fe183a6d3197e983ddfa4df882339b43aaf1d
Alexander Simonides <Alex-CS@users.noreply.github.com>
2022-07-29 16:10:19 -0700

    Simplify grid separation math

--- src/components/StitchGridLines.vue
+++ src/components/StitchGridLines.vue
@@ -18,9 +18,6 @@ export default defineComponent({
   },
   computed: {
 
-    gridSepFraction(): number {
-      return 1 / this.gridDensity;
-    },
 
   },
   methods: {
@@ -35,8 +32,8 @@ export default defineComponent({
     <line
       v-for="i in (gridDensity - 1)"
       :key="`grid-line:v-${i}`"
-      :x1="toPercent(gridSepFraction * i, 0)"
-      :x2="toPercent(gridSepFraction * i, 0)"
+      :x1="toPercent(i / gridDensity, 0)"
+      :x2="toPercent(i / gridDensity, 0)"
       y1="0%"
       y2="100%"
       class="vertical"
@@ -45,8 +42,8 @@ export default defineComponent({
     <line
       v-for="i in (gridDensity - 1)"
       :key="`grid-line:h-${i}`"
-      :y1="toPercent(gridSepFraction * i, 0)"
-      :y2="toPercent(gridSepFraction * i, 0)"
+      :y1="toPercent(i / gridDensity, 0)"
+      :y2="toPercent(i / gridDensity, 0)"
       x1="0%"
       x2="100%"
       class="horizontal"
